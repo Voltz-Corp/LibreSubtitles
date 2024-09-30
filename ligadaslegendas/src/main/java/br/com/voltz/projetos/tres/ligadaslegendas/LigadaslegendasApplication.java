68fdddcd42ca3fadaeb55d125b74587911d71938
Sophia <deletrasopa@gmail.com>
2024-09-30 09:15:17 -0300

    adding backend structure

---
+++ ligadaslegendas/src/main/java/br/com/voltz/projetos/tres/ligadaslegendas/LigadaslegendasApplication.java
@@ -0,0 +1,13 @@
+package br.com.voltz.projetos.tres.ligadaslegendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LigadaslegendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LigadaslegendasApplication.class, args);
+	}
+
+}
---
+++ ligadaslegendas/src/test/java/br/com/voltz/projetos/tres/ligadaslegendas/LigadaslegendasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.voltz.projetos.tres.ligadaslegendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LigadaslegendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
